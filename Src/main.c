678c94e4848dbf16888a0ef50138167f9f58ada0
Crazy <17147265+wwng2333@users.noreply.github.com>
2024-02-14 18:45:52 +0800

    switch to MDK Community

--- Src/main.c
+++ Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -40,113 +40,113 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define VERSION       "v1.8"
+#define VERSION "v1.8"
 #define N_MOSFET
 
-#define TEMP_MIN      150       // min selectable temperature
-#define TEMP_MAX      400       // max selectable temperature
-#define TEMP_DEFAULT  320       // default start setpoint
-#define TEMP_SLEEP    150       // temperature in sleep mode
-#define TEMP_BOOST     50       // temperature increase in boost mode
-#define TEMP_STEP      10       // rotary encoder temp change steps
+#define TEMP_MIN 150     // min selectable temperature
+#define TEMP_MAX 400     // max selectable temperature
+#define TEMP_DEFAULT 320 // default start setpoint
+#define TEMP_SLEEP 150   // temperature in sleep mode
+#define TEMP_BOOST 50    // temperature increase in boost mode
+#define TEMP_STEP 10     // rotary encoder temp change steps
 
-#define TEMP200       216       // temperature at ADC = 200
-#define TEMP280       308       // temperature at ADC = 280
-#define TEMP360       390       // temperature at ADC = 360
-#define TEMPCHP       30        // chip temperature while calibration
-#define TIPMAX        8         // max number of tips
-#define TIPNAMELENGTH 6         // max length of tip names (including termination)
-#define TIPNAME       "T12-K"   // default tip name
+#define TEMP200 216     // temperature at ADC = 200
+#define TEMP280 308     // temperature at ADC = 280
+#define TEMP360 390     // temperature at ADC = 360
+#define TEMPCHP 30      // chip temperature while calibration
+#define TIPMAX 8        // max number of tips
+#define TIPNAMELENGTH 6 // max length of tip names (including termination)
+#define TIPNAME "T12-K" // default tip name
 
-#define TIME2SLEEP     5        // time to enter sleep mode in minutes
-#define TIME2OFF      15        // time to shut off heater in minutes
-#define TIMEOFBOOST   40        // time to stay in boost mode in seconds
+#define TIME2SLEEP 5   // time to enter sleep mode in minutes
+#define TIME2OFF 15    // time to shut off heater in minutes
+#define TIMEOFBOOST 40 // time to stay in boost mode in seconds
 
-#define TIME2SETTLE   950       // time in microseconds to allow OpAmp output to settle
-#define SMOOTHIE      0.05      // OpAmp output smooth factor (1=no smoothing; 0.05 default)
-#define PID_ENABLE    false     // enable PID control
-#define BEEP_ENABLE   true      // enable/disable buzzer
-#define MAINSCREEN    0         // type of main screen (0: big numbers; 1: more infos)
+#define TIME2SETTLE 950  // time in microseconds to allow OpAmp output to settle
+#define SMOOTHIE 0.05    // OpAmp output smooth factor (1=no smoothing; 0.05 default)
+#define PID_ENABLE false // enable PID control
+#define BEEP_ENABLE true // enable/disable buzzer
+#define MAINSCREEN 0     // type of main screen (0: big numbers; 1: more infos)
 
-#define EEPROM_IDENT  0xE76C   // to identify if EEPROM was written by this program
+#define EEPROM_IDENT 0xE76C // to identify if EEPROM was written by this program
 
-#if defined (P_MOSFET)         // P-Channel MOSFET
-  #define HEATER_ON   255
-  #define HEATER_OFF  0
-  #define HEATER_PWM  255 - Output
-#elif defined (N_MOSFET)       // N-Channel MOSFET
-  #define HEATER_ON   0
-  #define HEATER_OFF  255
-  #define HEATER_PWM  Output
+#if defined(P_MOSFET) // P-Channel MOSFET
+#define HEATER_ON 255
+#define HEATER_OFF 0
+#define HEATER_PWM 255 - Output
+#elif defined(N_MOSFET) // N-Channel MOSFET
+#define HEATER_ON 0
+#define HEATER_OFF 255
+#define HEATER_PWM Output
 #else
-  #error Wrong MOSFET type!
+#error Wrong MOSFET type!
 #endif
 
-double aggKp=11, aggKi=0.5, aggKd=1;
-double consKp=11, consKi=3, consKd=5;
+double aggKp = 11, aggKi = 0.5, aggKd = 1;
+double consKp = 11, consKi = 3, consKd = 5;
 
-uint16_t  DefaultTemp = TEMP_DEFAULT;
-uint16_t  SleepTemp   = TEMP_SLEEP;
-uint8_t   BoostTemp   = TEMP_BOOST;
-uint8_t   time2sleep  = TIME2SLEEP;
-uint8_t   time2off    = TIME2OFF;
-uint8_t   timeOfBoost = TIMEOFBOOST;
-uint8_t   MainScrType = MAINSCREEN;
-bool      PIDenable   = PID_ENABLE;
-bool      beepEnable  = BEEP_ENABLE;
+uint16_t DefaultTemp = TEMP_DEFAULT;
+uint16_t SleepTemp = TEMP_SLEEP;
+uint8_t BoostTemp = TEMP_BOOST;
+uint8_t time2sleep = TIME2SLEEP;
+uint8_t time2off = TIME2OFF;
+uint8_t timeOfBoost = TIMEOFBOOST;
+uint8_t MainScrType = MAINSCREEN;
+bool PIDenable = PID_ENABLE;
+bool beepEnable = BEEP_ENABLE;
 
-uint16_t  CalTemp[TIPMAX][4] = {TEMP200, TEMP280, TEMP360, TEMPCHP};
-char      TipName[TIPMAX][TIPNAMELENGTH] = {TIPNAME};
-uint8_t   CurrentTip   = 0;
-uint8_t   NumberOfTips = 1;
+uint16_t CalTemp[TIPMAX][4] = {TEMP200, TEMP280, TEMP360, TEMPCHP};
+char TipName[TIPMAX][TIPNAMELENGTH] = {TIPNAME};
+uint8_t CurrentTip = 0;
+uint8_t NumberOfTips = 1;
 
-const char *SetupItems[]       = { "Setup Menu", "Tip Settings", "Temp Settings",
-                                   "Timer Settings", "Control Type", "Main Screen",
-                                   "Buzzer", "Information", "Return" };
-const char *TipItems[]         = { "Tip:", "Change Tip", "Calibrate Tip", 
-                                   "Rename Tip", "Delete Tip", "Add new Tip", "Return" };
-const char *TempItems[]        = { "Temp Settings", "Default Temp", "Sleep Temp", 
-                                   "Boost Temp", "Return" };
-const char *TimerItems[]       = { "Timer Settings", "Sleep Timer", "Off Timer", 
-                                   "Boost Timer", "Return" };
-const char *ControlTypeItems[] = { "Control Type", "Direct", "PID" };
-const char *MainScreenItems[]  = { "Main Screen", "Big Numbers", "More Infos" };
-const char *StoreItems[]       = { "Store Settings ?", "No", "Yes" };
-const char *SureItems[]        = { "Are you sure ?", "No", "Yes" };
-const char *BuzzerItems[]      = { "Buzzer", "Disable", "Enable" };
-const char *DefaultTempItems[] = { "Default Temp", "deg C" };
-const char *SleepTempItems[]   = { "Sleep Temp", "deg C" };
-const char *BoostTempItems[]   = { "Boost Temp", "deg C" };
-const char *SleepTimerItems[]  = { "Sleep Timer", "Minutes" };
-const char *OffTimerItems[]    = { "Off Timer", "Minutes" };
-const char *BoostTimerItems[]  = { "Boost Timer", "Seconds" };
-const char *DeleteMessage[]    = { "Warning", "You cannot", "delete your", "last tip!" };
-const char *MaxTipMessage[]    = { "Warning", "You reached", "maximum number", "of tips!" };
+const char *SetupItems[] = {"Setup Menu", "Tip Settings", "Temp Settings",
+                            "Timer Settings", "Control Type", "Main Screen",
+                            "Buzzer", "Information", "Return"};
+const char *TipItems[] = {"Tip:", "Change Tip", "Calibrate Tip",
+                          "Rename Tip", "Delete Tip", "Add new Tip", "Return"};
+const char *TempItems[] = {"Temp Settings", "Default Temp", "Sleep Temp",
+                           "Boost Temp", "Return"};
+const char *TimerItems[] = {"Timer Settings", "Sleep Timer", "Off Timer",
+                            "Boost Timer", "Return"};
+const char *ControlTypeItems[] = {"Control Type", "Direct", "PID"};
+const char *MainScreenItems[] = {"Main Screen", "Big Numbers", "More Infos"};
+const char *StoreItems[] = {"Store Settings ?", "No", "Yes"};
+const char *SureItems[] = {"Are you sure ?", "No", "Yes"};
+const char *BuzzerItems[] = {"Buzzer", "Disable", "Enable"};
+const char *DefaultTempItems[] = {"Default Temp", "deg C"};
+const char *SleepTempItems[] = {"Sleep Temp", "deg C"};
+const char *BoostTempItems[] = {"Boost Temp", "deg C"};
+const char *SleepTimerItems[] = {"Sleep Timer", "Minutes"};
+const char *OffTimerItems[] = {"Off Timer", "Minutes"};
+const char *BoostTimerItems[] = {"Boost Timer", "Seconds"};
+const char *DeleteMessage[] = {"Warning", "You cannot", "delete your", "last tip!"};
+const char *MaxTipMessage[] = {"Warning", "You reached", "maximum number", "of tips!"};
 
-volatile uint8_t  a0, b0, c0, d0;
-volatile bool     ab0;
-volatile int      count, countMin, countMax, countStep;
-volatile bool     handleMoved;
+volatile uint8_t a0, b0, c0, d0;
+volatile bool ab0;
+volatile int count, countMin, countMax, countStep;
+volatile bool handleMoved;
 
-uint16_t  SetTemp, ShowTemp, gap, Step;
-double    Input, Output, Setpoint, RawTemp, CurrentTemp, ChipTemp;
+uint16_t SetTemp, ShowTemp, gap, Step;
+double Input, Output, Setpoint, RawTemp, CurrentTemp, ChipTemp;
 
-uint16_t  Vcc, Vin;
+uint16_t Vcc, Vin;
 
-bool      inSleepMode = false;
-bool      inOffMode   = false;
-bool      inBoostMode = false;
-bool      inCalibMode = false;
-bool      isWorky     = true;
-bool      beepIfWorky = true;
-bool      TipIsPresent= true;
+bool inSleepMode = false;
+bool inOffMode = false;
+bool inBoostMode = false;
+bool inCalibMode = false;
+bool isWorky = true;
+bool beepIfWorky = true;
+bool TipIsPresent = true;
 
-uint32_t  sleepmillis;
-uint32_t  boostmillis;
-uint32_t  buttonmillis;
-uint8_t   goneMinutes;
-uint8_t   goneSeconds;
-uint8_t   SensorCounter = 255;
+uint32_t sleepmillis;
+uint32_t boostmillis;
+uint32_t buttonmillis;
+uint8_t goneMinutes;
+uint8_t goneSeconds;
+uint8_t SensorCounter = 255;
 
 /* USER CODE END PD */
 
@@ -172,24 +172,28 @@ void getChipTemp(void);
 uint16_t encoder_value = 200, tim_now, tim_last;
 uint16_t ADC_Buf[4] = {0};
 
-uint16_t map(uint16_t value, uint16_t inMin, uint16_t inMax, uint16_t outMin, uint16_t outMax) 
+uint16_t map(uint16_t value, uint16_t inMin, uint16_t inMax, uint16_t outMin, uint16_t outMax)
 {
   return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
 
 // calculates real temperature value according to ADC reading and calibration values
-void calculateTemp() {
-  if      (RawTemp < 200) CurrentTemp = map (RawTemp,   0, 200, 21, CalTemp[CurrentTip][0]);
-  else if (RawTemp < 280) CurrentTemp = map (RawTemp, 200, 280, CalTemp[CurrentTip][0], CalTemp[CurrentTip][1]);
-  else                    CurrentTemp = map (RawTemp, 280, 360, CalTemp[CurrentTip][1], CalTemp[CurrentTip][2]);
+void calculateTemp()
+{
+  if (RawTemp < 200)
+    CurrentTemp = map(RawTemp, 0, 200, 21, CalTemp[CurrentTip][0]);
+  else if (RawTemp < 280)
+    CurrentTemp = map(RawTemp, 200, 280, CalTemp[CurrentTip][0], CalTemp[CurrentTip][1]);
+  else
+    CurrentTemp = map(RawTemp, 280, 360, CalTemp[CurrentTip][1], CalTemp[CurrentTip][2]);
 }
 
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -224,57 +228,57 @@ int main(void)
   MX_SPI1_Init();
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
-	Activate_ADC();
-	LL_DMA_ConfigAddresses(DMA1, LL_DMA_CHANNEL_1, LL_ADC_DMA_GetRegAddr(ADC1, LL_ADC_DMA_REG_REGULAR_DATA), (uint32_t)&ADC_Buf, LL_DMA_DIRECTION_PERIPH_TO_MEMORY);
-	LL_DMA_SetDataLength(DMA1, LL_DMA_CHANNEL_1, 4);
-	LL_DMA_EnableChannel(DMA1, LL_DMA_CHANNEL_1);
-	SEGGER_RTT_Init();
-	SEGGER_RTT_printf(0, "Hello world!\r\n");
-//	LL_TIM_EnableCounter(TIM1);
-//	LL_TIM_EnableAllOutputs(TIM1);
-//	LL_TIM_CC_EnableChannel(TIM1, LL_TIM_CHANNEL_CH1);
-//	LL_TIM_CC_EnableChannel(TIM1, LL_TIM_CHANNEL_CH2);
-	LL_TIM_EnableAllOutputs(TIM2);
-	LL_TIM_CC_EnableChannel(TIM2, LL_TIM_CHANNEL_CH1);
-	LL_TIM_DisableCounter(TIM16);
-	LL_TIM_EnableAllOutputs(TIM16);
-	LL_TIM_CC_EnableChannel(TIM16, LL_TIM_CHANNEL_CH1);
-	
-	u8g2_Setup_ssd1306_128x64_noname_f(&u8g2, U8G2_R0, u8x8_byte_4wire_hw_spi, u8x8_stm32_gpio_and_delay);
-	u8g2_InitDisplay(&u8g2);
-	u8g2_SetPowerSave(&u8g2, 0);
-	LL_TIM_SetCounter(TIM1, 1000);
-	tim_last = LL_TIM_GetCounter(TIM1);
+  Activate_ADC();
+  LL_DMA_ConfigAddresses(DMA1, LL_DMA_CHANNEL_1, LL_ADC_DMA_GetRegAddr(ADC1, LL_ADC_DMA_REG_REGULAR_DATA), (uint32_t)&ADC_Buf, LL_DMA_DIRECTION_PERIPH_TO_MEMORY);
+  LL_DMA_SetDataLength(DMA1, LL_DMA_CHANNEL_1, 4);
+  LL_DMA_EnableChannel(DMA1, LL_DMA_CHANNEL_1);
+  SEGGER_RTT_Init();
+  SEGGER_RTT_printf(0, "Hello world!\r\n");
+  //	LL_TIM_EnableCounter(TIM1);
+  //	LL_TIM_EnableAllOutputs(TIM1);
+  //	LL_TIM_CC_EnableChannel(TIM1, LL_TIM_CHANNEL_CH1);
+  //	LL_TIM_CC_EnableChannel(TIM1, LL_TIM_CHANNEL_CH2);
+  LL_TIM_EnableAllOutputs(TIM2);
+  LL_TIM_CC_EnableChannel(TIM2, LL_TIM_CHANNEL_CH1);
+  LL_TIM_DisableCounter(TIM16);
+  LL_TIM_EnableAllOutputs(TIM16);
+  LL_TIM_CC_EnableChannel(TIM16, LL_TIM_CHANNEL_CH1);
+
+  u8g2_Setup_ssd1306_128x64_noname_f(&u8g2, U8G2_R0, u8x8_byte_4wire_hw_spi, u8x8_stm32_gpio_and_delay);
+  u8g2_InitDisplay(&u8g2);
+  u8g2_SetPowerSave(&u8g2, 0);
+  LL_TIM_SetCounter(TIM1, 1000);
+  tim_last = LL_TIM_GetCounter(TIM1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-		LL_ADC_REG_StartConversion(ADC1);
-		LL_ADC_REG_SetDMATransfer(ADC1, LL_ADC_REG_DMA_TRANSFER_UNLIMITED);
-        getChipTemp();
-		MainScreen(&u8g2);
-//		tim_now = LL_TIM_GetCounter(TIM1);
-//		if(tim_last != tim_now)
-//		{
-//			if(tim_last > tim_now)
-//			{
-//				encoder_value += 5;
-//				LL_TIM_EnableCounter(TIM16);
-//			}
-//			else
-//			{
-//				encoder_value -= 5;
-//				LL_TIM_DisableCounter(TIM16);
-//			}
-//			tim_last = tim_now;
-//		}
-		LL_mDelay(10);
-//		LL_TIM_EnableCounter(TIM2);
-//		LL_mDelay(500);
-//		LL_TIM_DisableCounter(TIM2);
-//		LL_mDelay(500);
+    LL_ADC_REG_StartConversion(ADC1);
+    LL_ADC_REG_SetDMATransfer(ADC1, LL_ADC_REG_DMA_TRANSFER_UNLIMITED);
+    getChipTemp();
+    MainScreen(&u8g2);
+    //		tim_now = LL_TIM_GetCounter(TIM1);
+    //		if(tim_last != tim_now)
+    //		{
+    //			if(tim_last > tim_now)
+    //			{
+    //				encoder_value += 5;
+    //				LL_TIM_EnableCounter(TIM16);
+    //			}
+    //			else
+    //			{
+    //				encoder_value -= 5;
+    //				LL_TIM_DisableCounter(TIM16);
+    //			}
+    //			tim_last = tim_now;
+    //		}
+    LL_mDelay(10);
+    //		LL_TIM_EnableCounter(TIM2);
+    //		LL_mDelay(500);
+    //		LL_TIM_DisableCounter(TIM2);
+    //		LL_mDelay(500);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -283,19 +287,19 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   LL_FLASH_SetLatency(LL_FLASH_LATENCY_2);
-  while(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_2)
+  while (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_2)
   {
   }
 
   /* HSI configuration and activation */
   LL_RCC_HSI_Enable();
-  while(LL_RCC_HSI_IsReady() != 1)
+  while (LL_RCC_HSI_IsReady() != 1)
   {
   }
 
@@ -303,7 +307,7 @@ void SystemClock_Config(void)
   LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLLM_DIV_1, 8, LL_RCC_PLLR_DIV_2);
   LL_RCC_PLL_Enable();
   LL_RCC_PLL_EnableDomain_SYS();
-  while(LL_RCC_PLL_IsReady() != 1)
+  while (LL_RCC_PLL_IsReady() != 1)
   {
   }
 
@@ -312,7 +316,7 @@ void SystemClock_Config(void)
 
   /* Sysclk activation on the main PLL */
   LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
-  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
   {
   }
 
@@ -327,45 +331,45 @@ void SystemClock_Config(void)
 
 /* USER CODE BEGIN 4 */
 
-int stdout_putchar (int ch) 
+int stdout_putchar(int ch)
 {
   return (SEGGER_RTT_PutChar(0, ch));
 }
 
-void MainScreen(u8g2_t *u8g2) 
+void MainScreen(u8g2_t *u8g2)
 {
-	char sprintf_tmp[8] = {0};
-	u8g2_FirstPage(u8g2);
-	u8g2_SetFontMode(u8g2, 1);
-	u8g2_SetFontDirection(u8g2, 0);
-	do
-	{
-		u8g2_SetFont(u8g2, u8g2_font_9x15_te);
-		u8g2_DrawStr(u8g2, 0, 10, "SET:");
-		u8g2_DrawStr(u8g2, 40, 10, "200");
-		u8g2_DrawStr(u8g2, 83, 10, "  OFF");
-		u8g2_DrawStr(u8g2, 0, 62, "T12-KU");
-		sprintf(sprintf_tmp, "%.1fV", ((float)ADC_Buf[0]/4096)*3.3*((47+4.7)/4.7));
-		u8g2_DrawStr(u8g2, 83, 62, sprintf_tmp);
-		
-		u8g2_SetFont(u8g2, u8g2_font_freedoomr25_mn);
-		sprintf(sprintf_tmp, "%d", encoder_value);
-		u8g2_DrawStr(u8g2, 37, 45, sprintf_tmp);
-	} while (u8g2_NextPage(u8g2));
+  char sprintf_tmp[8] = {0};
+  u8g2_FirstPage(u8g2);
+  u8g2_SetFontMode(u8g2, 1);
+  u8g2_SetFontDirection(u8g2, 0);
+  do
+  {
+    u8g2_SetFont(u8g2, u8g2_font_9x15_te);
+    u8g2_DrawStr(u8g2, 0, 10, "SET:");
+    u8g2_DrawStr(u8g2, 40, 10, "200");
+    u8g2_DrawStr(u8g2, 83, 10, "  OFF");
+    u8g2_DrawStr(u8g2, 0, 62, "T12-KU");
+    sprintf(sprintf_tmp, "%.1fV", ((float)ADC_Buf[0] / 4096) * 3.3 * ((47 + 4.7) / 4.7));
+    u8g2_DrawStr(u8g2, 83, 62, sprintf_tmp);
+
+    u8g2_SetFont(u8g2, u8g2_font_freedoomr25_mn);
+    sprintf(sprintf_tmp, "%d", encoder_value);
+    u8g2_DrawStr(u8g2, 37, 45, sprintf_tmp);
+  } while (u8g2_NextPage(u8g2));
 }
 
 void getChipTemp(void)
 {
-    uint32_t ADC_VCC;
-    ADC_VCC = __LL_ADC_CALC_VREFANALOG_VOLTAGE(ADC_Buf[2], LL_ADC_RESOLUTION_12B); //
-    ChipTemp = __LL_ADC_CALC_TEMPERATURE(ADC_VCC, ADC_Buf[1], LL_ADC_RESOLUTION_12B);
+  uint32_t ADC_VCC;
+  ADC_VCC = __LL_ADC_CALC_VREFANALOG_VOLTAGE(ADC_Buf[2], LL_ADC_RESOLUTION_12B); //
+  ChipTemp = __LL_ADC_CALC_TEMPERATURE(ADC_VCC, ADC_Buf[1], LL_ADC_RESOLUTION_12B);
 }
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -377,14 +381,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
